3ec61246f37e53801644d49c9366cddb4a442cc4
Wulcato <60317536+wulcato@users.noreply.github.com>
2024-07-24 17:42:32 +0300

    Update stylelint.config.js

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,7 +5,7 @@ module.exports = {
             4,
             {
                 "except": ["block"],
-                "message": "Please use 2 spaces for indentation.",
+                "message": "Please use 2 spaces for indentation. - Lütfen 2 karakter kullanın.",
                 "severity": "warning"
             }
         ],
