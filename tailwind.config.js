615335a7690ea69ed2dd344889daa984626f7949
Wulcato <60317536+wulcato@users.noreply.github.com>
2024-07-24 17:42:36 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('@tailwindcss/postcss7-compat/colors')
 
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: true, // or 'media' or 'class'
   theme: {
     extend: {},
     colors: {
